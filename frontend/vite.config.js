9c0f401bfd8460ef27a2f7e4503c21e098e99a9b
surya3444 <isuryakarthikvarma@gmail.com>
2026-03-06 14:54:46 +0530

    configured vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/', // Add this! (Use './' if you are on a cPanel/shared host)
   plugins: [
     react(),
     tailwindcss(),
